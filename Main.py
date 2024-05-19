d66ddacabab8fdcb992ed99d86a0c99a901f91a3
MrNadix <mrnadix@mrnadix.com>
2024-05-19 13:57:30 +0200

    Simple script

--- Main.py
+++ Main.py
@@ -7,55 +7,31 @@
 ########  #### ##     ## ##     ## ######## ##     ##       ##     #######   #######  ########  ######  
 
 import os
-import subprocess
-import glob
 
+#Create playlist in a BMW CCC compatible way
 def main():
-    #Temporal stuff. Just for today.
+    print("Write drive route")
+    driveLoc = input(">> ")
 
-    songs = {}
+    subFolders = [f.name for f in os.scandir(driveLoc) if f.is_file()]
 
-    exitDirCreation = False
-
-    while (not exitDirCreation):
-        print("Select dir name")
-        print("Write end to finish")
-        dirName = input(">> ")
-    
-        if (dirName == 'end'):
-            exitDirCreation = True
-        else:
-            songs[dirName] = []
-            exit = False
-            print("Add spotify lists and songs")
-            print("Write end to finish")
-            while (not exit):
-                song = input(">> ")
-                if (song == 'end'):
-                    exit = True
-                else:
-                    songs[dirName].append(song)
-    
-    for dirKey in songs.keys():
-        try:
-            os.mkdir("output/" + dirKey)
-        except:
-            next
-
-    for song in songs[dirKey]:
-        os.system("cd 'output/" + dirKey +  "' && ../../libs/spotdl-4.2.4-linux " + song)
-    
-    bimmerPlayListCreation(dirKey)
+    for detectedFolder in subFolders:
+        print("Detected: " + detectedFolder)
+        bimmerPlayListCreation(detectedFolder, driveLoc)
 
 
-def bimmerPlayListCreation(dirKey):
-    dir = "output/" + dirKey
-    songs = glob.glob(dir + "/*.mp3")
+def bimmerPlayListCreation(dirKey, driveLoc):
+    dir = driveLoc + "/" + dirKey
+    songs = [f.name for f in os.scandir(driveLoc) if f.is_file()]
+    try:
+        os.remove(dir + ".m3u")
+    except:
+        next
 
-    playList = open("output/" + dirKey + ".m3u", "w")
+    playList = open(dir + ".m3u", "w")
 
     for song in songs:
-        playList.write(song.replace("/", "\\").replace("output\\", "") + "\r\n")
+        playList.write(song.replace("/", "\\") + "\r\n")
 
 if __name__ == '__main__':
     main()
